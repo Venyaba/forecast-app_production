292ff94bb89ed7e5ee9defdf74520102469319b5
venya <wenseslasbar@gmail.com>
2019-04-05 14:37:18 +0300

    add prod

---
+++ precache-manifest.73ab3ab1a042065133d4575a5d917db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a882c77ae3b972ce226",
+    "url": "/static/js/main.900bc904.chunk.js"
+  },
+  {
+    "revision": "a01940c4f76fcdf91571",
+    "url": "/static/js/2.6d4170bb.chunk.js"
+  },
+  {
+    "revision": "4a882c77ae3b972ce226",
+    "url": "/static/css/main.4804adb7.chunk.css"
+  },
+  {
+    "revision": "a01940c4f76fcdf91571",
+    "url": "/static/css/2.d39287ba.chunk.css"
+  },
+  {
+    "revision": "5d7167da7b7a0edb819447f150016882",
+    "url": "/index.html"
+  }
+];
